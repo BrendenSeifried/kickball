57b5bc661c204cec55a1147f2e7bb025c00fc64a
BrendenSeifried <brendenseifried@gmail.com>
2022-03-24 17:16:10 -0700

    started a globo gym theme more css friday probably

--- src/views/LandingPage/LandingPage.js
+++ src/views/LandingPage/LandingPage.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Landing.css';
 
 export default function LandingPage() {
   return (
-    <div>Welcome to the League</div>
+    <h1>Globo Gym welcomes you to <span>The League</span></h1>
   );
 }
 

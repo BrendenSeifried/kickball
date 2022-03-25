4db26e28a83806414339f537862cafcdee88e572
BrendenSeifried <brendenseifried@gmail.com>
2022-03-24 17:31:18 -0700

    background inserted

--- src/views/LandingPage/LandingPage.js
+++ src/views/LandingPage/LandingPage.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './Landing.css';
+import globo from './globo.png';
 
 export default function LandingPage() {
   return (
-    <h1>Globo Gym welcomes you to <span>The League</span></h1>
+    <>
+      <h1>Globo Gym welcomes you to <span>The League</span></h1>
+    
+      <img src={`./${globo}`} />
+    </>
   );
 }
 
